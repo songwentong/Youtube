577aee8690bfc94060b2a8234f98e2b2a4c67405
宋文通 <songwentong@mobimagic.com>
2018-10-09 14:46:07 +0800

    DemoViewController

---
+++ Youtube/Source/More/DemoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DemoViewController.swift
+//  Youtube
+//
+//  Created by 宋文通 on 2018/10/9.
+//  Copyright © 2018 Wentong Song. All rights reserved.
+//
+
+import UIKit
+
+class DemoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
