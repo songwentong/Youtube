c82665e09d2a39360595e038453093686958a7b7
宋文通 <songwentong@mobimagic.com>
2018-10-23 10:20:36 +0800

    API

--- Youtube/Source/Gernal/Networking/API/playlists.swift
+++ Youtube/Source/Gernal/Networking/API/playlists.swift
@@ -7,11 +7,14 @@
 //
 
 import Foundation
+import Alamofire
 extension Networking{
-    static func playlists( channelId:String, finish:@escaping (PlayListResult)->Void, failed:@escaping (NetWorkingError)->Void) -> Void {
+    @discardableResult
+    static func playlists( channelId:String, finish:@escaping (PlayListResult)->Void, failed:@escaping (NetWorkingError)->Void) -> DataRequest {
         let parameter = ["part":"snippet,contentDetails","maxResults":"25","channelId":channelId,"key":youtuBeAPIKey]
         let task = Networking.default.network_request(Networking.default.url(methodName: .youtube_search_list),parameters:parameter)
         task.convert(finished: finish, failed: failed)
+        return task
         //GET https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=surfing&key={YOUR_API_KEY}
     }
 }
