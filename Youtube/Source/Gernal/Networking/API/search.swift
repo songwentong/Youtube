5a24f4316a4ebeaae304e27e257995fc998cfcdb
宋文通 <songwentong@mobimagic.com>
2018-10-24 17:13:14 +0800

    URLSessionTask

--- Youtube/Source/Gernal/Networking/API/search.swift
+++ Youtube/Source/Gernal/Networking/API/search.swift
@@ -7,11 +7,22 @@
 //
 
 import Foundation
+extension URLSessionTask{
+    func responseBlock() -> Void {
+        
+    }
+    func convert() -> Void {
+        
+    }
+}
 extension Networking{
     static func search( q:String, finish:@escaping (SearchResult)->Void, failed:@escaping (NetWorkingError)->Void) -> Void {
         let parameter = ["part":"snippet","maxResults":"25","q":q,"key":youtuBeAPIKey]
         let task = Networking.default.network_request(Networking.default.url(methodName: .search),parameters:parameter)
         task.convert(finished: finish, failed: failed)
+        URLSession.shared.dataTask(with: URL.init(fileURLWithPath: "")) { (data, response, error) in
+            
+        }
         //GET https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=surfing&key={YOUR_API_KEY}
     }
 }
